ea0aad79aa7be8a293eae490e61a43690098bfe0
wangliting <wangliting@e-autolife.com>
2018-11-26 09:29:30 +0800

    web:替换成后台接口

--- pages/group/group.js
+++ pages/group/group.js
@@ -1,4 +1,7 @@
 // pages/group/group.js
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+
 var app = getApp();
 Page({
 
@@ -13,7 +16,8 @@ Page({
     var that = this;
     // 全民砍价
     wx.request({
-      url: app.globalData.subDomain + '/shop/goods/list',
+      // url: app.globalData.subDomain + '/shop/goods/list',
+      url: api.IndexUrlBanner,
       success: function(res) {
         if(res.data.code == 0){
          that.list = res.data.data;
